6621166fabac8b28975d8d5bc1a4850e43fc3702
Paula Rodrigues <pullynnhah@icloud.com>
2023-08-14 17:26:54 -0300

    :tada: start project

---
+++ src/protocols.ts
@@ -0,0 +1,64 @@
+import { Payment, Ticket } from '@prisma/client';
+
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+export type ViaCEPAddress = {
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+};
+
+export type ViaCEPAddressError = {
+  error: boolean;
+};
+
+export type ViaCEPAddressResponse = {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+};
+
+export type AddressEnrollment = {
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+  error?: string;
+};
+
+export type RequestError = {
+  status: number;
+  data: object | null;
+  statusText: string;
+  name: string;
+  message: string;
+};
+
+export type CreateTicketParams = Omit<Ticket, 'id' | 'createdAt' | 'updatedAt'>;
+
+export type CardPaymentParams = {
+  issuer: string;
+  number: number;
+  name: string;
+  expirationDate: Date;
+  cvv: number;
+};
+
+export type PaymentParams = Omit<Payment, 'id' | 'createdAt' | 'updatedAt'>;
+
+export type InputTicketBody = {
+  ticketTypeId: number;
+};
